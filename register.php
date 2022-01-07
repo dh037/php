1fe5ef4726612d16da6774127ab5453a36dfa954
(dhh037) <(dhh037@gmail.com)>
2022-01-07 14:30:01 +0900

    $user_email 추가

--- register.php
+++ register.php
@@ -5,7 +5,7 @@ require_once 'database.php';
 $user_id = $_POST['user_id'];
 $user_pwd = $_POST['user_pwd'];
 $user_phone = $_POST['user_phone'];
-
+$user_email = $_POST['user_email'];
 
 $conn = mysqli_connect(db_host,db_user,db_password,db_name);
 $sql = "SELECT * FROM board_user WHERE user_id='{$user_id}' AND user_pwd='{$user_pwd}'";
