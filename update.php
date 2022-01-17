50264582212ecece05de66836b67e2d85b530103
(dhh037) <(dhh037@gmail.com)>
2022-01-17 17:46:17 +0900

    2022/01/17 (월) 17:46

--- update.php
+++ update.php
@@ -10,15 +10,16 @@ $user_addr2 = $_POST['address2'];
 // $user_use_yn = $_POST['user_use_yn'];
 $user_id = $_POST['id'];
 
+
+//      hobbies='{$hobbies}',      user_use_yn='{$user_use_yn}'
 $conn = mysqli_connect(db_host,db_user,db_password,db_name);
 $sql = "UPDATE board_user SET
      user_email='{$user_email}',
---      hobbies='{$hobbies}',
      user_zip='{$user_zip}',
      user_addr1='{$user_addr1}',
-     user_addr2='{$user_addr2}',
---      user_use_yn='{$user_use_yn}'
+     user_addr2='{$user_addr2}'
       WHERE user_id='{$user_id}'";
+
 
 $result = mysqli_query($conn,$sql);
 
